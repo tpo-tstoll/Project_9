0cfac817119da03010af0e8a641b3c134dfc16ef
Erica Collier <ercollie@travelers.com>
2021-03-30 16:53:07 -0400

    added single course route

--- routes/courses.js
+++ routes/courses.js
@@ -19,8 +19,31 @@ router.get('/courses', asyncHandler(async(req, res) => {
         attributes: {
             exclude: ['createdAt', 'updatedAt'],
         }
-    });
-    console.log(res.json(courses));
-}));
 
-module.exports = router;
+      });
+      console.log(res.json(courses));
+  }));
+
+//GET route for a single course - commenting out function asyncHandler until middleware is setup
+app.get('/api/courses/:id', /* asyncHandler**/ (async (req, res) => {
+    const course = await Course.findByPK(req.params.id, {
+      include: {
+        model: User
+      },
+    });
+    if (course === 'undefined') {
+      const err = new Error('Looks like we don\'t offer that course');
+      err.status = 404;
+      throw err;
+    } else {
+      res.status(200).json({ course: {
+        id: course.id,
+        title: course.title,
+        description: course.description,
+        estimatedTime: course.estimatedTime,
+        materialsNeeded: course.materialsNeeded
+      } } );
+    }
+  }));
+
+  module.exports = router;
