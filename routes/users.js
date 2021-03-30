aa108e98f6bdf8523327c5fded99ec18a61e4e16
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-03-30 15:25:49 -0500

    Handled merge conflicts, added Post User Route

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+
+const router = express.Router();
+const {User} = require('../models');
+const bcrypt = require('bcryptjs');
+
+// Handler function to wrap each route
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try {
+            await cb(req, res, next)
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+// Post route to create a new user
+router.post('/users', asyncHandler(async(req, res) => {
+    try {
+        let user = req.body;
+        user.password = bcrypt.hashSync(user.password)
+        await User.create(user);
+        await res.location('/').status(201).end();;
+    } catch (error) {
+        console.log('ERROR: ', error.name);
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+          const errors = error.errors.map(err => err.message);
+          res.status(400).json({ errors });   
+        } else {
+          throw error;
+        }
+    } 
+}))
+
+module.exports = router;
