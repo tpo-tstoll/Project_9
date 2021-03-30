ecf4c146c785f427f122f0bb067826c89fee08f7
Erica Collier <ercollie@travelers.com>
2021-03-30 16:23:57 -0400

    setup get route for single course

--- routes/Courses.js
+++ routes/Courses.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express.Router()
+const { User, Course } = require('../models/User')
+
 // const { asyncHandler } = require('../middleware/asynchandler') - see note 
-// const { Course } = require('../models/courses') - commenting out importing the course model as this has not been created yet
 
 
 //GET route for a single course - commenting out function asyncHandler until middleware is setup
 app.get('/api/courses/:id', /* asyncHandler**/ (async (req, res) => {
-    const course = await Course(req.params.id);
-    if (course) {
-      res.json(course).status(200).end();
+    const course = await Course.findByPK(req.params.id, {
+      include: {
+        model: User
+      },
+      res.status(200).end()
+    });
+    if (course === 'undefined') {
+      const err = new Error('Looks like we don\'t offer that course');
+      err.status = 404;
+      throw err;
     } else {
-      res.status(404).json({ message: "Looks like we don't offer that course" });
+      res.status(404).json({ course: {
+        id: course.id,
+        title: course.title,
+        description: course.description,
+        estimatedTime: course.estimatedTime,
+        materialsNeeded: course.materialsNeeded
+      } } );
     }
   }));
