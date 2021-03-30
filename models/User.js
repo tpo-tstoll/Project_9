22962e36547845f687ca7e4c6ff02698008882eb
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-03-30 11:02:23 -0500

    Add user class

---
+++ models/User.js
@@ -0,0 +1,70 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class User extends Model {}
+    User.init({
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A first name is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a first name'
+                }
+            }
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A last name is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a last name'
+                }
+            }
+        },
+        emailAddress: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                msg: 'Sorry, this email is already in use.'
+            },
+            validate: {
+                notNull: {
+                    msg: 'An email is required'
+                },
+                isEmail: {
+                    msg: 'Please provide a valid email address'
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A password is required'
+                },
+                notEmpty: {
+                    msg: 'Please provide a password'
+                }
+            }
+        }
+    }, {sequelize});
+
+    User.associate = (models) => {
+        User.hasMany(models.Course, {
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false
+            }
+        }) 
+    }
+
+    return User;
+};
