d59822d978583de62ba1879b01643b6c6e765e67
Erica Collier <ercollie@travelers.com>
2021-03-31 14:19:10 -0400

    update for test

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -7,20 +7,6 @@ exports.authenticateUser = async (req, res, next) => {
   const credentials = auth(req);
   let message;
 
-<<<<<<< HEAD
-  if (credentials) {
-    const user = await User.findOne({ where: { emailAddress: credentials.emailAddress } });
-    if (user) {
-      const authenticated = bcrypt.compareSync(credentials.pass, user.password);
-      if (authenticated) {
-        console.log(`${user.emailAddress} has been successfully authenticated!`);
-        req.currentUser = user;
-      } else {
-        message = `${user.emailAddress} could not be authenticated`;
-      }
-    } else {
-      message = 'User could not be located.';
-=======
     if (credentials) {
         const user = await User.findOne({ where: {emailAddress: credentials.name} })
         if (user) {
@@ -36,27 +22,16 @@ exports.authenticateUser = async (req, res, next) => {
         }
     } else {
         message = 'Access Denied, Goodbye..';
->>>>>>> f4b568b3ec386f608f5fee91fb8effe47b304b4e
-    }
+    },
   } else {
     message = 'Auth header not found';
   }
 
-<<<<<<< HEAD
-  if (message) {
-    console.warn(message);
-    res.status(401).json({ message: 'Access Denied, Goodbye..' });
-  } else {
-    next();
-  }
-};
-=======
     if (message) {
         console.warn(message);
         res.status(401).json(message);
     } else {
         next();
     }
-
 }
->>>>>>> f4b568b3ec386f608f5fee91fb8effe47b304b4e
+
