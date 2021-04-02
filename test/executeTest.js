da572a1e62b2bcd0de93d3c0f1e6b9956917bedf
Erica Collier <ercollie@travelers.com>
2021-04-02 11:13:11 -0400

    test works

--- test/executeTest.js
+++ test/executeTest.js
@@ -1,25 +1,23 @@
-const test = require('th_unit09_unit_tests');
-const Procmonrest = require('procmonrest');
+const test = require('th_unit09_unit_tests')
+const Procmonrest = require('procmonrest')
 
-    
- describe('an end-to-end test', function () {
-   this.timeout(10000)
-   
-   const serverProcess = new Procmonrest({
-   command: 'npm start',
-   waitFor: /listening on port \d{4}/i
-   });
-  
-   before(() => {
-     return serverProcess.start();
-   })
-  
-   after(() => {
-     if (serverProcess.running) {
-       return serverProcess.stop();
-     }
-   })
 
-   test.meetsRun(); 
-   
- })
+//  describe('an end-to-end test', function () {
+//    this.timeout(10000)
+
+//    const serverProcess = new Procmonrest({
+//    command: 'npm start',
+//    waitFor: /listening on port \d{4}/i
+//    })
+
+//    before(() => {
+//      return serverProcess.start()
+//    })
+
+//    after(() => {
+//      if (serverProcess.running) {
+//        return serverProcess.stop()
+//      }
+//    })
+
+test.meetsRun()
