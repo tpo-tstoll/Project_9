7c69aa2573530cdd333bd880f0e9f40ea9f2af31
Erica Collier <ercollie@travelers.com>
2021-04-05 12:01:28 -0400

    pushing updates

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -5,52 +5,38 @@ const { Course, User } = require('../../models');
 
 router.get('/', asyncHandler(async (req, res) => {
   const courses = await Course.findAll({
-    include: {
-      model: User
-    },
     attributes: {
       exclude: ['createdAt', 'updatedAt']
-    }
-
-  }),
-  const courseSearch = courses.map((courses) => ({
-    course: {
-      id: course.id,
-      title: course.title,
-      description: course.description,
-      estimatedTime: course.estimatedTime,
-      materialsNeeded: course.materialsNeeded,
-      userId: course.userId,
-      userFname: course.User.firstName,
-      userLname: course.User.lastName,
-      userEmail: course.User.emailAddress
     },
-  }))
-  res.json({ courses, courseSearch });
+    include: {
+      model: User,
+      attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      }
+    }
+  })
+  res.json(courses);
 }));
 
 // GET route for a single course - commenting out function asyncHandler until middleware is setup
 router.get('/:id', asyncHandler(async (req, res) => {
   const course = await Course.findByPk(req.params.id, {
-    include: {
-      model: User
-    }
-  });
+     attributes: {
+        exclude: ['createdAt', 'updatedAt']
+      },
+      include: {
+        model: User,
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        }
+      }
+    })
   if (course === undefined || course === null) {
     const err = new Error('Looks like we don\'t offer that course');
     res.status(400).json(err.message);
     throw err;
   } else {
-    res.status(200).json({
-      course: {
-        id: course.id,
-        title: course.title,
-        description: course.description,
-        estimatedTime: course.estimatedTime,
-        materialsNeeded: course.materialsNeeded,
-        userId: course.userId
-      },
-    });
+    res.status(200).json( course );
   }
 }));
 
