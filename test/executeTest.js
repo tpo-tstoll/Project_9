43b5ef5befdb45a248627a625bd24664789f68e1
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-04-06 11:34:56 -0500

    Hashing moved to user model

--- test/executeTest.js
+++ test/executeTest.js
@@ -1,23 +1,25 @@
-const test = require('th_unit09_unit_tests')
-const Procmonrest = require('procmonrest')
+const test = require('th_unit09_unit_tests');
+const Procmonrest = require('procmonrest');
 
+    
+ describe('an end-to-end test', function () {
+   this.timeout(5000)
+   
+   const serverProcess = new Procmonrest({
+   command: 'npm start',
+   waitFor: /listening on port \d{4}/i
+   });
+  
+   before(() => {
+     return serverProcess.start();
+   })
+  
+   after(() => {
+     if (serverProcess.running) {
+       return serverProcess.stop();
+     }
+   })
 
-//  describe('an end-to-end test', function () {
-//    this.timeout(10000)
-
-//    const serverProcess = new Procmonrest({
-//    command: 'npm start',
-//    waitFor: /listening on port \d{4}/i
-//    })
-
-//    before(() => {
-//      return serverProcess.start()
-//    })
-
-//    after(() => {
-//      if (serverProcess.running) {
-//        return serverProcess.stop()
-//      }
-//    })
-
-test.meetsRun()
+   test.meetsRun(); 
+   
+ })
